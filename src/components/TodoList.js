f56674910133c8fb120b1b9304447c364dbf8903
Oleh <eyeofatiger@yandex.ua>
2020-03-04 16:20:21 +0200

    Make first changing with structure

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = ()=> {
+    return(
+        <ul>
+            <li>Learn React</li>
+            <li>Build awesome app</li>
+        </ul>
+    );
+};
+
+export default TodoList;
