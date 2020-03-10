1aab53090818046e2f6c95849eea8edd6d5d3594
Oleh <eyeofatiger@yandex.ua>
2020-03-10 20:02:47 +0200

    Finished with refactoring

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -2,42 +2,34 @@ import React from 'react';
 import './ItemStatusFilter.css';
 
 export default class ItemStatusFilter extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { buttons:[
+      {name: 'All'},
+      {name: 'Active'},
+      {name: 'Done'}
+    ]};
+  }
   
   render(){
     const {filter, onStatusChange} = this.props;
-     let  putClassAll = '',
-          putClassActive = '',
-          putClassDone = '';
-    
-    if(filter === 'All'){
-      putClassAll = 'btn-info';
-      putClassActive = 'btn-outline-secondary';
-      putClassDone = 'btn-outline-secondary';
-    } 
-    
-    if(filter === 'Active'){
-      putClassAll = 'btn-outline-secondary';
-      putClassActive = 'btn-info';
-      putClassDone = 'btn-outline-secondary';
-    } 
 
-    if(filter === 'Done'){
-      putClassAll = 'btn-outline-secondary';
-      putClassActive = 'btn-outline-secondary';
-      putClassDone = 'btn-info';
-    } 
+    const button = this.state.buttons.map((el)=>{
+      const rez = filter === el.name;
+      const putClass = rez ? 'btn-info' : 'btn-outline-secondary';
+      return(
+        <button type="button"
+                className={`btn ${putClass}`}
+                onClick={()=>onStatusChange(el.name)}
+                key={el.name}>{el.name}</button>
+      );
+    });
 
     return (
       <div className="btn-group">
-        <button type="button"
-                className={`btn ${putClassAll}`}
-                onClick={()=>onStatusChange('All')}>All</button>
-        <button type="button"
-                className={`btn ${putClassActive}`}
-                onClick={()=>onStatusChange('Active')}>Active</button>
-        <button type="button"
-                className={`btn ${putClassDone}`}
-                onClick={()=>onStatusChange('Done')}>Done</button>
+        {button}
       </div>
     );
   }
